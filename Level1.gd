71cd26cbcc9b45c5212e5871f9303ef0ff43922c
unknown <edwin.kramp@mailbox.tu-dresden.de>
2024-06-02 21:08:20 +0200

    now able to place grass in level editor and reset level

--- Level1.gd
+++ Level1.gd
@@ -5,9 +5,11 @@ extends Node2D
 @onready var tile_map = $"MapViewContainer/MapView/TileMap"
 @onready var canvas_layer = $UISide
 @onready var _1 = $"UISide/Control/VBoxContainer/1"
+@onready var _6 = $"UISide/Control/VBoxContainer/6"
 @onready var _2 = $"UISide/Control/VBoxContainer/2"
 @onready var _3 = $"UISide/Control/VBoxContainer/3"
 @onready var _4 = $"UISide/Control/VBoxContainer/4"
+@onready var _5 = $"UISide/Control/VBoxContainer/5"
 @onready var boat = $"MapViewContainer/MapView/boat"
 @onready var map_view = $"MapViewContainer/MapView"
 
@@ -22,30 +24,38 @@ var is_drawing:bool = false
 func _ready():
 	# Verbinde das `pressed`-Signal mit der `_on_Button_pressed`-Methode
 	_1.connect("pressed", Callable(self, "_on_Button_1_pressed"))
+	_6.connect("pressed", Callable(self, "_on_Button_6_pressed"))
 	_2.connect("pressed", Callable(self, "_on_Button_2_pressed"))
 	_3.connect("pressed", Callable(self, "_on_Button_3_pressed"))
 	_4.connect("pressed", Callable(self, "_on_Button_4_pressed"))
+	_5.connect("pressed", Callable(self, "_on_Button_5_pressed"))
 	tile_map.atlas_coords = Vector2i(3,2)
 	boat_start_position = boat.global_position
 
 func _input(event):
 	# Überprüfe, ob der Event ein Mausklick ist
+	global_mouse_position = event.global_position
+	
+	if global_mouse_position.x > map_view.size.x:
+		return
+	
 	if event is InputEventMouseButton:
-		global_mouse_position = event.global_position
 		if port_mode and not tile_mode:
 			is_drawing = false
 			_process_port_placement()
 		if tile_mode and not port_mode:
+			_process_tile_change()
 			is_drawing = event.pressed
 			
 	if event is InputEventMouseMotion and is_drawing:
-		global_mouse_position = event.global_position
 		if tile_mode and not port_mode:
 			_process_tile_change()
 		
 		
-		
-		
+
+func _restart_scene():
+	get_tree().reload_current_scene()
+
 func _process_tile_change():
 	tile_map._on_tile_selected(global_mouse_position)
 	
@@ -54,6 +64,11 @@ func _process_port_placement():
 		
 func _on_Button_1_pressed():
 	tile_map.atlas_coords = Vector2i(3,2)
+	tile_mode = true
+	port_mode = false
+	
+func _on_Button_6_pressed():
+	tile_map.atlas_coords = Vector2i(0,1)
 	tile_mode = true
 	port_mode = false
 	
@@ -70,6 +85,9 @@ func _on_Button_4_pressed():
 	boat = boat_scene.instantiate()
 	boat.global_position = boat_start_position
 	map_view.add_child(boat)
+	
+func _on_Button_5_pressed():
+	_restart_scene()
 	
 	
 
