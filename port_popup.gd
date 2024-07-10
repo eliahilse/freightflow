dabbe3ca02f9661af73c1f90a9f673d562de951f
Elia <elia@eliahilse.com>
2024-07-10 19:45:10 +0200

    feat: port slot settings, delete port

--- port_popup.gd
+++ port_popup.gd
@@ -1,6 +1,7 @@
 extends Panel
 
 signal operation_entered(operation: int, value: int)
+signal delete_port(operation: int, value: int)
 
 enum PortOperation {
 	ADD,
@@ -10,15 +11,21 @@ enum PortOperation {
 	MOD,
 }
 
-@onready var operand = $HBoxContainer/VBoxContainer/HBoxContainer/Operand
-@onready var number = $HBoxContainer/VBoxContainer/HBoxContainer/Number
+@onready var operand = $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer2/Operand
+@onready var deleteButton = $HBoxContainer/VBoxContainer/HBoxContainer2/Delete
+@onready var slotSelect = $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/Slot
+@onready var number = $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer3/Number
 @onready var apply_button = $HBoxContainer/VBoxContainer/MarginContainer/ApplyButton
 
 func _ready():
 	apply_button.connect("pressed", Callable(self, "_on_apply_button_pressed"))
-
+	deleteButton.connect("pressed", Callable(self, "_on_delete_button_pressed"))
+	
 	for operation in PortOperation.keys():
 		operand.add_item(operation)
+		
+	for i in range(4):
+		slotSelect.add_item(str(i+1))
 	
 	DisplayServer.virtual_keyboard_show('')
 	
@@ -34,15 +41,23 @@ func _on_number_text_changed():
 		if regex.search(current_text.substr(0, i + 1)):
 			new_text = current_text.substr(0, i + 1)
 	
+
+	
 	if new_text != current_text:
 		number.text = new_text
 		number.set_caret_column(new_text.length())
 
 func _on_apply_button_pressed():
 	var selected_operation = operand.selected
+	var selected_slot = slotSelect.selected
 	var input_value = int(number.text) if number.text.is_valid_int() else 0
-	emit_signal("operation_entered", selected_operation, input_value)
+	emit_signal("operation_entered", selected_operation, input_value, selected_slot)
 	queue_free()
+
+func _on_delete_button_pressed():
+	emit_signal("delete_port")
+	queue_free()
+
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
