a96e145c7d6a751ea5a299d520312064b5b1543d
Elia <elia@eliahilse.com>
2024-07-11 11:43:03 +0200

    wip: landing

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen max-w-[100vw] overflow-x-clip flex-col items-center justify-between p-8 md:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex max-w-[100vw] pl-6 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Sandbox game teaching programming flow concepts&nbsp;
+          PoC for a sandbox game teaching programming flow concepts&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full max-w-[100vw] items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -72,7 +72,38 @@ export default function Home() {
             Write us an E-Mail
           </p>
         </a>
-
+        <a
+            href="https://github.com/eliahilse/freightflow"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Repository{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          View the code
+          </p>
+        </a>
+        <a
+            href="/team"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Team{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          People behind FreightFlow
+          </p>
+        </a>
 
       </div>
     </main>
